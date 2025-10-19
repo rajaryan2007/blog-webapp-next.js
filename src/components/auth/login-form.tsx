624f9267ec41a129527fe0d802a67f20fb8338f2
rajaryan2007 <rajaryan149150@gmail.com>
2025-10-19 19:45:03 +0530

    compelete the auth

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { z } from "zod";
-import { FormField } from "../ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
@@ -34,17 +34,38 @@ const form = useForm<LoginFormvalues>({
 
 })
 
-    return(
-        <Form {...form} >
-        <form className="space-y-4">
+    return <Form {...form} >
+         <form className="space-y-4">
             <FormField
             control={form.control}
-            >
+            name="email"
+            render={({field})=>(
+                <FormItem>
+                    <FormLabel>Email </FormLabel>
+                    <FormControl>
+                        <input className="rounded-xl p-2" placeholder="Enter your email" {...field} />
+                    </FormControl>
+                </FormItem>
+            )}    
+            />
+            <FormField
+            control={form.control}
+            name="password"
+            render={({field})=>(
+                <FormItem>
+                    <FormLabel>password </FormLabel>
+                    <FormControl>
+                        <input className="rounded-xl p-2" placeholder="Enter your password" type="password" {...field} />
+                    </FormControl>
+                </FormItem>
+            )}    
+            />
+            
 
-            </FormField>
-        </form>
-        </Form>
-    )
+
+         </form>
+    </Form>
+    
 }
 
 export default LoginForm;
--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,9 +1,53 @@
+'use client'
+
+import {z} from "zod";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormField} from "@/components/ui/form";
+
+const registerSchema = z.object({
+    name:z.string(),
+    email:z.string().email("please enter the valid email address"),
+    Password:z.string().min(6, "please enter your password"),
+    confirmPassword:z.string().min(6, "please enter your password"),
+}).refine((data)=>data.Password === data.confirmPassword,{
+      message:"Password is incorrect",
+      path:["confirmPassword","password"],
 
 
-function RegisterForm() {
-    return(
-        <div>register form</div>
-    )
+});
+
+type RegisterFormvalue = z.infer<typeof registerSchema>
+
+
+function RegisterForm(){
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const form = useForm<RegisterFormvalue>({
+        resolver:zodResolver(registerSchema),
+        defaultValues:{
+        name:'',
+        Password:'',
+        email:'',
+        confirmPassword: ''
+        }
+    })
+
+
+    return <Form {...form}>
+        <form className='space-y-4' >
+            <FormField render={} name={}>
+
+
+            </FormField>
+
+        </form>
+
+
+
+    </Form>
+
 }
 
 export default RegisterForm;
+
