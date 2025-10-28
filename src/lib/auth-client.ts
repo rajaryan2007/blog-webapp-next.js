c1c6ad4eff73ff8d876473a71daff873b3b5ea43
rajaryan2007 <rajaryan149150@gmail.com>
2025-10-29 01:23:53 +0530

    complete

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,17 @@
 
 import {createAuthClient} from "better-auth/react"
 
+const baseURL =
+  process.env.BASE_URL ||
+  (typeof window !== 'undefined'
+    ? window.location.origin
+    : 'http://localhost:3000');
 
 export const authClient = createAuthClient({
-   baseURL:process.env.BASE_URL,
+  baseURL,
+});
 
-})
+
+
 
 export const {signUp,signIn,signOut,useSession } = authClient;
